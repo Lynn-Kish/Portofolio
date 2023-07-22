d07126108805302ff88cc2d1f02fd30c35c3cb8f
lunakish <lunakish6@gmail.com>
2023-07-22 22:25:56 +0300

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function toggleMobileMenu() {
-    const menuListDesktop = document.querySelector('.menu-list-desktop');
-    menuListDesktop.classList.toggle('show-menu');
-  }             
+
+const hamburger= document.querySelector(".hamburger");
+const navMenu= document.querySelector(".nav-menu");
+
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active"); 
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener
+  ("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }))
